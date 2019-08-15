99131334a93bf155ae7359358ba52d8b012b6a56
mdashikulislam <ashik713832@gmail.com>
2019-08-16 00:43:03 +0600

    Initial File

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<div id="paypal-button"></div>
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<script>
+    paypal.Button.render({
+        // Configure environment
+        env: 'sandbox',
+        client: {
+            sandbox: 'AWH2YAbzGMKECSHBOw0_BVRFX9tclShcg512AHr9bMNBBG7esnuYwsXPTf0HagiyszMCbyOyhqJoeo9o',
+            production: 'demo_production_client_id'
+        },
+        // Customize button (optional)
+        locale: 'en_US',
+        style: {
+            size: 'small',
+            color: 'blue',
+            shape: 'pill',
+        },
+        // Set up a payment
+        payment: function(data, actions) {
+            return actions.payment.create({
+                transactions: [{
+                    amount: {
+                        total: '20',
+                        currency: 'USD'
+                    }
+                }]
+            });
+        },
+        // Execute the payment
+        onAuthorize: function(data, actions) {
+            return actions.payment.execute().then(function() {
+                // Show a confirmation message to the buyer
+                window.alert('Thank you for your purchase!');
+            });
+        }
+    }, '#paypal-button');
+</script>
